d8f5a78d3bf515a5a0133bc919abd75d80e9771e
Rob Miles <rob.miles@volvo.com>
2018-06-25 12:08:24 -0400

    notes for further development of builder

--- test/ui/command/TestInvokerBuilder.java
+++ test/ui/command/TestInvokerBuilder.java
@@ -10,7 +10,17 @@ public class TestInvokerBuilder
 	@Test
 	public void test()
 	{
-		fail("Not yet implemented");
+
+
+		Invoker invoker = new Invoker();
+		InvokerBuilder ib = new InvokerBuilder();
+		
+		ib.constructInvoker();  //this guy sets all the invoker.cmd# variables to new Reload(); or whatever
+		
+		assertTrue(invoker.cmd1 instanceof Reload());  //check that you're invoker.cmd1 instance variable points to the Reload() class (which is an extension of Command interface)
+		
+		
+		//  repeat assertTrue for each of the buttons/invoker.cmdx instances
 	}
 
 }
