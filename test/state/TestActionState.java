9f0a7e2859da92234e471bd8e60dd9d734d9ae6a
Rob Miles <rob.miles@volvo.com>
2018-06-26 10:35:06 -0400

    Adding Test Classes

---
+++ test/state/TestActionState.java
@@ -0,0 +1,24 @@
+package state;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TestActionState
+{
+
+//	+ evaluate()  //only for AIState
+//	+ search()
+//	+ acquire()
+//	+ attack
+//	+ reload
+//	+ dead(); //(am I dead)
+//	+ search()
+
+	@Test
+	public void test()
+	{
+		fail("Not yet implemented");
+	}
+
+}
---
+++ test/state/TestOutOfAmmoState.java
@@ -0,0 +1,16 @@
+package state;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TestOutOfAmmoState
+{
+
+	@Test
+	public void test()
+	{
+		fail("Not yet implemented");
+	}
+
+}
